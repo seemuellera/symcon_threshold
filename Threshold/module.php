c7a1014b2db7214da5ed53287dbbfc1e1b60e111
Andreas Seemueller <andreas.seemueller@gmail.com>
2021-04-21 21:25:35 +0200

    Added debug messages to isEqual compare

--- Threshold/module.php
+++ Threshold/module.php
@@ -150,7 +150,16 @@ class Threshold extends IPSModule {
 
 	}
 	
+	// Version 1.0
 	protected function LogMessage($message, $severity = 'INFO') {
+		
+		$logMappings = Array();
+		// $logMappings['DEBUG'] 	= 10206; Deactivated the normal debug, because it is not active
+		$logMappings['DEBUG'] 	= 10201;
+		$logMappings['INFO']	= 10201;
+		$logMappings['NOTIFY']	= 10203;
+		$logMappings['WARN'] 	= 10204;
+		$logMappings['CRIT']	= 10205;
 		
 		if ( ($severity == 'DEBUG') && ($this->ReadPropertyBoolean('DebugOutput') == false )) {
 			
@@ -158,8 +167,7 @@ class Threshold extends IPSModule {
 		}
 		
 		$messageComplete = $severity . " - " . $message;
-		
-		IPS_LogMessage($this->ReadPropertyString('Sender') . " - " . $this->InstanceID, $messageComplete);
+		parent::LogMessage($messageComplete, $logMappings[$severity]);
 	}
 
 	public function RefreshInformation() {
@@ -289,12 +297,16 @@ class Threshold extends IPSModule {
 	
 	protected function CheckIsEqual($inputValue) {
 		
+		$this->LogMessage("Running Numerical comparison: Is Equal / value: $inputValue / threshold: " . $this->ReadPropertyFloat("NumericalThreshold"), "DEBUG");
+		
 		if ($inputValue == $this->ReadPropertyFloat("NumericalThreshold") ) {
-			
+		
+			$this->LogMessage("Value is equal", "DEBUG");
 			$this->UpdateAlertState(true);
 		}
 		else {
 			
+			$this->LogMessage("Value is not equal", "DEBUG");
 			$this->UpdateAlertState(false);
 		}
 	}
